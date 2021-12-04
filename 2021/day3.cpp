0178b04cd8c670347b4f09419494a4c638b36d40
Tom Pescatore <tpescatore11@gmail.com>
2021-12-04 18:17:22 -0500

    streamlined program

--- 2021/day3.cpp
+++ 2021/day3.cpp
@@ -5,28 +5,28 @@
 #include <iostream>
 #include <bitset>
 #include <iterator>
-#include <cstring>
 
-int day_3_part_1();
-
-int day_3_part_2();
+int day_3_part_1(std::vector<std::string> data);
+int day_3_part_2(std::vector<std::string> data);
+std::vector<std::string> get_input();
+std::string most_common_or_least_common_by_bit(std::vector<std::string> data, bool most);
 
 char most_common_bit_at(std::vector<std::string> v, int pos);
 
 int main(int argc, char const *argv[])
 {
-	auto part_1_answer = day_3_part_1();
+	std::vector<std::string> data = get_input();
+
+	auto part_1_answer = day_3_part_1(data);
+	auto part_2_answer = day_3_part_2(data);
 
 	std::cout << "Day 3 Part 1: " << part_1_answer << std::endl;
-
-	auto part_2_answer = day_3_part_2();
-
 	std::cout << "Day 3 Part 2: " << part_2_answer << std::endl;
 
 	return 0;
 }
 
-int day_3_part_1() {
+std::vector<std::string> get_input() {
 	std::string word;
 	std::ifstream day_3_input ("day_3_input.txt");
 
@@ -36,23 +36,13 @@ int day_3_part_1() {
 		data.push_back(word);
 	}
 
-	std::string s = data.at(0);
-	std::vector<int> counts(s.size(), 0);
-	for (std::string e : data) {
-		for (int i = 0; i < e.size(); i++) {
-			if (e.at(i) == '1')
-				counts[i] += 1;
-			else
-				counts[i] -= 1;
-		}
-	}
+	return data;
+}
 
+int day_3_part_1(std::vector<std::string> data) {
 	std::string binary;
-	for (auto e : counts) {
-		if (e > 0)
-			binary.append("1");
-		else
-			binary.append("0");
+	for (int i = 0; i < data.at(0).size(); i++) {
+		binary.push_back(most_common_bit_at(data, i));
 	}
 
 	std::bitset<12> b3(binary);
@@ -60,62 +50,42 @@ int day_3_part_1() {
 	return b3.to_ulong() * b3.flip().to_ulong();
 }
 
-int day_3_part_2() {
-	std::string word;
-	std::ifstream day_3_input ("day_3_input.txt");
+int day_3_part_2(std::vector<std::string> data) {
 
-	std::vector<std::string> data;
+	std::string oxygen_value = most_common_or_least_common_by_bit(data, true);
+	std::string co2_value = most_common_or_least_common_by_bit(data, false);
 
-	while (day_3_input >> word) {
-		data.push_back(word);
-	}
-
-	std::vector<std::string> oxygen_values;
-	std::vector<std::string> co2_values;
-
-	char most_common_bit = most_common_bit_at(data, 0);
-
-	for (auto e : data) {
-		if (e.at(0) == most_common_bit) {
-			oxygen_values.push_back(e);
-		}
-		else 
-			co2_values.push_back(e);
-	}
-
-	int pos = 1;
-	while (oxygen_values.size() > 1) {
-		most_common_bit = most_common_bit_at(oxygen_values, pos);
-
-		for (auto it = begin(oxygen_values); it != end (oxygen_values); ) {
-			if (it -> at(pos) != most_common_bit)
-				it = oxygen_values.erase(it);
-			else
-				it++;
-		}
-
-		pos++;
-	}
-
-	pos = 1;
-	while (co2_values.size() > 1) {
-		most_common_bit = most_common_bit_at(co2_values, pos);
-
-		for (auto it = begin(co2_values); it != end (co2_values); ) {
-			if (it -> at(pos) == most_common_bit)
-				it = co2_values.erase(it);
-			else
-				it++;
-		}
-
-		pos++;
-	}
-
-	std::bitset<12> oxygen(oxygen_values.at(0));
-	std::bitset<12> co2(co2_values.at(0));
+	std::bitset<12> oxygen(oxygen_value);
+	std::bitset<12> co2(co2_value);
 
 	return oxygen.to_ulong() * co2.to_ulong();
 
+}
+
+std::string most_common_or_least_common_by_bit(std::vector<std::string> v, bool most) {
+	int pos = 0;
+
+	while (v.size() > 1) {
+		char most_common_bit = most_common_bit_at(v, pos);
+
+		for (auto it = begin(v); it != end(v); ) {
+			if (most) {
+				if (it -> at(pos) != most_common_bit)
+					it = v.erase(it);
+				else
+					it++;
+			}
+			else {
+				if (it -> at(pos) == most_common_bit)
+					it = v.erase(it);
+				else
+					it++;
+			}
+		}
+		pos++;
+	}
+
+	return v.at(0);
 }
 
 char most_common_bit_at(std::vector<std::string> v, int pos) {
@@ -131,10 +101,4 @@ char most_common_bit_at(std::vector<std::string> v, int pos) {
 		return '0';
 	return '1';
 }
-
-
-
-
-
-
 
