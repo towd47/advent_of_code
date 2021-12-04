f7aa31edbff3f189941db25538cb8bfd64176d34
Tom Pescatore <tpescatore11@gmail.com>
2021-12-04 17:21:31 -0500

    smoothed out sums of list

--- 2021/python/day1.py
+++ 2021/python/day1.py
@@ -6,15 +6,14 @@ lines = [int(line) for line in data]
 single_increases = 0
 three_sum_increases = 0
 
-last_sum = lines[0] + lines[1] + lines[2]
 for i in range(1, len(lines)):
 	if lines[i] > lines[i-1]:
 		single_increases += 1
 	if i > 2:
-		new_sum = last_sum - lines[i-3] + lines[i]
-		if new_sum > last_sum:
+		sum1 = sum(lines[i-4:i-1])
+		sum2 = sum(lines[i-3:i])
+		if sum2 > sum1:
 			three_sum_increases += 1
-		last_sum = new_sum
 
 print("Day 1 Part 1: ", single_increases)
 print("Day 1 Part 2: ", three_sum_increases)
