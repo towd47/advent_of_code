90275b9caa42c4166ec036be7a5e881be0f4c9e5
Tom Pescatore <tpescatore11@gmail.com>
2021-12-04 17:24:24 -0500

    removed unnecessary variables

--- 2021/python/day2.py
+++ 2021/python/day2.py
@@ -4,19 +4,16 @@ data = day2_input.readlines()
 horiz = 0
 depth = 0
 aim = 0
-depth2 = 0
 
 for line in data:
 	[direction, value] = line.split()
 	if direction == "forward":
 		horiz += int(value)
-		depth2 += aim * int(value)
+		depth += aim * int(value)
 	elif direction == "up":
-		depth -= int(value)
 		aim -= int(value)
 	else:
-		depth += int(value)
 		aim += int(value)
 
-print("Day 2 part 1: ", horiz * depth)
-print("Day 2 part 2: ", horiz * depth2)
+print("Day 2 part 1: ", horiz * aim)
+print("Day 2 part 2: ", horiz * depth)
