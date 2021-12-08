5a1755ab2b1289f69b57fdd9f1fef6af6f7e9e02
Tom Pescatore <tpescatore11@gmail.com>
2021-12-08 15:26:26 -0500

    day6 soln both parts - should have seen this solution quicker

---
+++ 2021/python/day6.py
@@ -0,0 +1,22 @@
+def total_fish(starting_fish, days):
+	fish = starting_fish.copy()
+	index = 0
+	new_fish_buffer = [0] * 2
+	for i in range(days):
+		new_fish_buffer.append(fish[index])
+		fish[index] += new_fish_buffer.pop(0)
+		index = (index + 1) % 7
+
+	return sum(fish) + sum(new_fish_buffer)
+
+day6_input = open("../day_6_input.txt")
+data = day6_input.readline().split(',')
+
+starting_fish = [0] * 7
+
+for x in data:
+	y = int(x)
+	starting_fish[y] += 1
+
+print(total_fish(starting_fish, 80))
+print(total_fish(starting_fish, 256))
