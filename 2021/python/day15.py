92d0c5796a511feff7a1722dfe7728ed6dff79b5
Tom Pescatore <tpescatore11@gmail.com>
2021-12-17 09:26:23 -0500

    it takes several seconds and its messy and repetitive but it works

--- 2021/python/day15.py
+++ 2021/python/day15.py
@@ -27,8 +27,6 @@ cols = len(risks[0])
 
 score_grid[0][0] = risks[0][0]
 
-# for row in range(rows):
-# 	for col in range(cols):
 adj = adjacent_pts(0, 0, rows, cols)
 pts_to_check = [[0, 0]]
 while pts_to_check:
@@ -47,4 +45,37 @@ while pts_to_check:
 
 print(score_grid[rows - 1][cols - 1] - score_grid[0][0])
 
+big_risks = []
+for j in range(5):
+	for i in range(len(risks)):
+		new_risks = []
+		for k in range(5):
+			new_risk = list(map(lambda x: ((x + j + k) % 10 + 1) if (x + j + k > 9) else (x + j + k), risks[i]))
+			new_risks.extend(new_risk)
+		big_risks.append(new_risks)
 
+big_score_grid = []
+for x in range(len(big_risks[0])):
+	big_score_grid.append([10000] * len(big_risks))
+
+rows = len(big_risks)
+cols = len(big_risks[0])
+
+big_score_grid[0][0] = big_risks[0][0]
+
+adj = adjacent_pts(0, 0, rows, cols)
+pts_to_check = [[0, 0]]
+while pts_to_check:
+	ad = pts_to_check.pop(0)
+	ads_ads = adjacent_pts(ad[0], ad[1], rows, cols)
+	for ads_ad in ads_ads:
+		if big_score_grid[ad[0]][ad[1]] > big_score_grid[ads_ad[0]][ads_ad[1]] + big_risks[ad[0]][ad[1]]:
+			big_score_grid[ad[0]][ad[1]] = big_score_grid[ads_ad[0]][ads_ad[1]] + big_risks[ad[0]][ad[1]]
+			for ads_ad in ads_ads:
+				if ads_ad not in pts_to_check:
+					pts_to_check.append(ads_ad)
+		elif big_score_grid[ad[0]][ad[1]] + big_risks[ads_ad[0]][ads_ad[1]] < big_score_grid[ads_ad[0]][ads_ad[1]]:
+			big_score_grid[ads_ad[0]][ads_ad[1]] = big_score_grid[ad[0]][ad[1]] + big_risks[ads_ad[0]][ads_ad[1]]
+			pts_to_check.append(ads_ad)		
+
+print(big_score_grid[rows-1][cols-1] - big_score_grid[0][0])
