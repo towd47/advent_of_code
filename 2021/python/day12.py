f75fedfa5acc3e26a31a35fc163374c13542f018
Tom Pescatore <tpescatore11@gmail.com>
2021-12-20 14:06:34 -0500

    day12 part 1 soln

---
+++ 2021/python/day12.py
@@ -0,0 +1,37 @@
+def find_path(cave_system, path, current_location):
+	paths = []
+	for i in cave_system[current_location]:
+		new_path = path + ',' + i
+		if i == 'end':
+			paths.append(new_path)
+		elif i.isupper() or i not in path:
+			paths.extend(find_path(cave_system, new_path, i))
+	return paths
+
+
+day12_input = open("../day_12_input.txt")
+data = day12_input.readlines()
+
+cave_system = {}
+
+for line in data:
+	line = line.strip()
+	key, val = line.split('-')
+	if key in cave_system:
+		l = cave_system[key]
+		l.append(val)
+		cave_system[key] = l
+	else:
+		cave_system[key] = [val]
+	if val in cave_system:
+		l = cave_system[val]
+		l.append(key)
+		cave_system[val] = l
+	else:
+		cave_system[val] = [key]
+
+path = 'start'
+
+
+paths = find_path(cave_system, path, path)
+print(len(paths))
