44bc6e2d8986a87b4d4bc1c2a6fc0bd4b6e94d5b
towd47 <tpescatore11@gmail.com>
2023-12-03 21:09:15 -0500

    small cleanup for day2 soln

--- 2023/2.py
+++ 2023/2.py
@@ -1,44 +1,47 @@
 import readInput
 
-def getRGBCounts(s):
-	s = s.split(":")[1]
-	a = s.split(";")
+def getMaxRGB(s):
+	games = s.split(";")
 
-	b = 0
-	r = 0
-	g = 0
+	rmax = 0
+	gmax = 0
+	bmax = 0
 
-	for x in a:
-		l = x.split(",")
-		for y in l:
-			y = y.strip()
-			num, char = y.split(" ")
+	for game in games:
+		colors = game.split(",")
+		for color in colors:
+			color = color.strip()
+			num, char = color.split()
 			num = int(num)
 			char = char[0]
 
-			if char == 'b' and num > b:
-				b = num
-			elif char == 'r' and num > r:
-				r = num
-			elif char == 'g' and num > g:
-				g = num
+			if char == 'b' and num > bmax:
+				bmax = num
+			elif char == 'r' and num > rmax:
+				rmax = num
+			elif char == 'g' and num > gmax:
+				gmax = num
 
-	return (r, g, b)
+	return (rmax, gmax, bmax)
 
 def validForPart1(r, g, b):
 	if r > 12 or g > 13 or b > 14:
 		return False
 	return True
 
-tot = 0
-tot2 = 0
-for line in readInput.yieldLines("2"):
-	r, g, b = getRGBCounts(line)
-	if validForPart1(r, g, b):
-		l = line.split(":")[0]
-		n = int("".join([x for x in l if x.isdigit()]))
-		tot += n
-	tot2 += r * g * b
+def day2():
+	tot = 0
+	tot2 = 0
+	for line in readInput.yieldLines("2"):
+		lineNum, rgbs = line.split(":")
+		r, g, b = getMaxRGB(rgbs)
+		if validForPart1(r, g, b):
+			n = int("".join([x for x in lineNum if x.isdigit()]))
+			tot += n
+		tot2 += r * g * b
 
-print(tot)
-print(tot2)
+	print(f'Part 1: {tot}')
+	print(f'Part 2: {tot2}')
+
+if __name__ == "__main__":
+	day2()
