17980d085ba16b3fd6215057e023f545f814ba1f
towd47 <tpescatore11@gmail.com>
2023-12-08 14:52:01 -0500

    day 2 p2 haskell solution

--- 2023/hask/2.hs
+++ 2023/hask/2.hs
@@ -4,13 +4,14 @@ import System.IO
 import Data.Function
 import Data.List
 import Data.Bool
-import Data.Text hiding ( map, lines)
+import Data.Text hiding ( map, lines, null, isSuffixOf, head)
 import Data.Text.Read
 import Prelude
 
 main = do
     s <- readFile "../inputs/2"
     print $ solve part1 s
+    print $ solve part2 s
 
 
 solve :: (String -> Maybe Int) -> String -> Int
@@ -28,16 +29,33 @@ part1 s | areValid (map unpack (listPulls pulls)) = Just (read (unpack (getGameN
         where
             (gameId:pulls) = getVals (pack s)
 
+part2 :: String -> Maybe Int
+part2 s = Just $ product (getMaxForEachColor (map unpack (listPulls pulls)) 0 0 0)
+        where
+            (_:pulls) = getVals (pack s)
+
+getMaxForEachColor :: [String] -> Int -> Int -> Int -> [Int]
+getMaxForEachColor (x:xs) r g b | "red" `isSuffixOf` x = getMaxForEachColor xs (max r (getDraws (pack x))) g b
+                                | "green" `isSuffixOf` x = getMaxForEachColor xs r (max g (getDraws (pack x))) b
+                                | "blue" `isSuffixOf` x = getMaxForEachColor xs r g (max b (getDraws (pack x)))
+getMaxForEachColor _ r g b = [r, g, b]
+
+getDraws pull = read (unpack num)
+            where
+                (num:_) = splitOn (pack " ") pull
+
+getGameNum :: Text -> Text
 getGameNum s = splitOn (pack " ") s !! 1
 
+getVals :: Text -> [Text]
 getVals = splitOn (pack ": ")
 
+areValid :: [String] -> Bool
 areValid = Data.List.all isValidAmount
 
 
 listPulls :: [Text] -> [Text]
 listPulls [s] = Data.List.concat (map (splitOn (pack ", ")) (splitOn (pack "; ") s))
--- [[1 green, 2 red, 3 blue], [1 blue, 4 red]]
 
 isValidAmount :: String -> Bool
 isValidAmount s = read (unpack x) <= getMax (unpack y)
