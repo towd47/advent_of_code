d3045a87cf50556d98cb73ac9a2eb39dab18a34d
towd47 <tpescatore11@gmail.com>
2023-12-10 09:15:31 -0500

    added read method

--- 2023/readInput.py
+++ 2023/readInput.py
@@ -2,3 +2,7 @@ def yieldLines(filename):
 	with open(f'inputs/{filename}', 'r') as f:
 		for line in f:
 			yield line
+
+def linesToList(filename):
+	with open(f'inputs/{filename}', 'r') as f:
+		return f.readlines()
