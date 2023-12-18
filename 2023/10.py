3ca2d8132d5f7948ed8f7dec09fe43d3fb471d64
towd47 <tpescatore11@gmail.com>
2023-12-17 22:46:24 -0500

    day 10 p2 wip

--- 2023/10.py
+++ 2023/10.py
@@ -45,93 +45,131 @@ def setOfAllEdges(chain):
 def allEdges(p1, p2):
 	edges = set()
 	if p1[0] == p2[0]:
-		if p1[0] > p2[0]: edgeRange = range(p2[1], p1[1] + 1)
+		if p1[1] > p2[1]: edgeRange = range(p2[1], p1[1] + 1)
 		else: edgeRange = range(p1[1], p2[1] + 1)
 		for i in edgeRange:
 			edges.add((p1[0], i))
 	else:
-		if p1[1] > p2[1]: edgeRange = range(p2[0], p1[0] + 1)
+		if p1[0] > p2[0]: edgeRange = range(p2[0], p1[0] + 1)
 		else: edgeRange = range(p1[0], p2[0] + 1)
 		for i in edgeRange:
-			edges.add((p1[1], i))
+			edges.add((i, p1[1]))
 	return edges
 
-lines = readInput.linesToList("10")
-strippedLines = []
-for line in lines:
-	line = line.strip()
-	strippedLines.append(line)
-lines = strippedLines
+def getGridAndSPos(f):
+		
+	lines = readInput.linesToList(f)
+	strippedLines = []
+	for line in lines:
+		line = line.strip()
+		strippedLines.append(line)
+	lines = strippedLines
 
-sPos = None
-for row, line in enumerate(lines):
-	for col, val in enumerate(line):
-		if val == "S":
-			sPos = (row, col)
+	sPos = None
+	for row, line in enumerate(lines):
+		for col, val in enumerate(line):
+			if val == "S":
+				sPos = (row, col)
+				break
+		if sPos:
 			break
-	if sPos:
-		break
+	return (lines, sPos)
 
-nodes = set()
-nodes.add(sPos)
+def getChain(lines, sPos):
+	nodes = set()
+	nodes.add(sPos)
+
+	rows = len(lines)
+	cols = len(lines[0])
+
+	chain = [sPos]
+
+	nodesToStartFrom = getAdj(sPos, rows, cols)
+
+	for node in nodesToStartFrom:
+		nodes.add(node)
+		chain.append(node)
+		adj = getAdjByLetter(node, lines[node[0]][node[1]])
+		if sPos not in adj:
+			chain = [sPos]
+			continue
+		while isValid(adj[0], rows, cols) and isValid(adj[1], rows, cols) and (adj[0] not in nodes or adj[1] not in nodes):
+			if adj[0] not in nodes:
+				chain.append(adj[0])
+				nodes.add(adj[0])
+				adj = getAdjByLetter(adj[0], lines[adj[0][0]][adj[0][1]])
+			elif adj[1] not in nodes:
+				chain.append(adj[1])
+				nodes.add(adj[1])
+				adj = getAdjByLetter(adj[1], lines[adj[1][0]][adj[1][1]])
+		if not isValid(adj[0], rows, cols) or not isValid(adj[1], rows, cols):
+			chain = [sPos]
+		else:
+			break
+	return chain
+	
+def applyDir(chainPoint, dir):
+	return (chainPoint[0] + dir[0], chainPoint[1] + dir[1])
+
+def newDir(l, dir):
+	if l == "-" or l == "|":
+		return dir
+	if l == "7" or l == "L" or l == "S":
+		if dir[0] == 0:
+			return rotateRight(dir)
+		return rotateLeft(dir)
+	if l == "J" or l == "F":
+		if dir[0] == 0:
+			return rotateLeft(dir)
+		return rotateRight(dir)
+	return (-1, -1)
+
+def rotateLeft(dir):
+	return (dir[1], dir[0] * -1)
+
+def rotateRight(dir):
+	return (dir[1] * -1, dir[0])
+
+lines, sPos = getGridAndSPos("10")
+chain = getChain(lines, sPos)
+print(int(len(chain) / 2))
 
 rows = len(lines[0])
 cols = len(lines)
 
-chain = [sPos]
+chainSet = setOfAllEdges(chain)
+chain = [(x, y, lines[x][y]) for (x, y) in chain]
+highest = (10000000, -1)
+for i, e in enumerate(chain):
+	if (e[0] < highest[0]):
+		highest = (e[0], i)
 
-nodesToStartFrom = getAdj(sPos, rows, cols)
+print(highest)
 
-for node in nodesToStartFrom:
-	nodes.add(node)
-	chain.append(node)
-	adj = getAdjByLetter(node, lines[node[0]][node[1]])
-	if sPos not in adj:
-		chain = [sPos]
-		continue
-	while isValid(adj[0], rows, cols) and isValid(adj[1], rows, cols) and (adj[0] not in nodes or adj[1] not in nodes):
-		if adj[0] not in nodes:
-			chain.append(adj[0])
-			nodes.add(adj[0])
-			adj = getAdjByLetter(adj[0], lines[adj[0][0]][adj[0][1]])
-		elif adj[1] not in nodes:
-			chain.append(adj[1])
-			nodes.add(adj[1])
-			adj = getAdjByLetter(adj[1], lines[adj[1][0]][adj[1][1]])
-	if not isValid(adj[0], rows, cols) or not isValid(adj[1], rows, cols):
-		chain = [sPos]
-	else:
-		break
+insidePoints = []
+startPoint = chain[highest[1]]
+dir = (1, 0)
 
-print(int(len(chain) / 2))
-
-doubledChain = [(x * 2, y * 2) for (x, y) in chain]
-
-chainSet = setOfAllEdges(doubledChain)
-
-edgeSet = set()
-for i in range(cols * 2):
-	edgeSet.add((0, i))
-	edgeSet.add((rows * 2 - 1, i))
-for i in range(rows * 2):
-	edgeSet.add((i, 0))
-	edgeSet.add((i, cols * 2 - 1))
+for i in range(highest[0], len(chain) + highest[0]):
+	i = i % len(chain)
+	if i != highest[0]:
+		dir = newDir(chain[i][2], dir)
+	insidePoints.append(applyDir(chain[i], dir))
 
 visitedSet = set()
 visitedSet.update(chainSet)
-for x in edgeSet:
+for x in insidePoints:
 	if x in visitedSet:
 		continue
 	nodesToCheck = [x]
 	while nodesToCheck:
 		node = nodesToCheck.pop()
 		visitedSet.add(node)
-		adj = getAdj(node, rows * 2, cols * 2)
+		adj = getAdj(node, rows, cols)
 		for y in adj:
 			if y not in visitedSet:
 				nodesToCheck.append(y)
-print(len(visitedSet) / (rows * 2 * cols * 2))
 
-
+print(len(visitedSet) - len(chain))
 
 
