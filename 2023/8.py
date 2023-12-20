0fb6f703eef182e1b1984cd043ef0e25784a7861
Tom Pescatore <tpescatore11@gmail.com>
2023-12-20 07:04:24 -0800

    WOW I WAY OVERTHUNK THIS :upsidedownsmilyface:

--- 2023/8.py
+++ 2023/8.py
@@ -1,4 +1,5 @@
 import readInput
+import math
 
 def part1(turns, nodes):
     currentNode = "AAA"
@@ -12,23 +13,47 @@ def part1(turns, nodes):
         steps += 1
     print(steps)
 
-lines = readInput.yieldLines("8")
+def part2(turns, nodes, startNodes):
+    zs = []
+    for cn in startNodes:
+        z = findPattern(turns, nodes, cn)
+        zs.append(z)
+    print(math.lcm(*zs))
 
-turns = next(lines)
-turns = turns.strip()
-next(lines)
 
-nodes = dict()
-p2StartNodes = []
-for line in lines:
-    line = line.strip()
-    node, nextNodes = line.split(" = ")
-    next1, next2 = nextNodes.split(", ")
-    next1 = next1.lstrip("(")
-    next2 = next2.rstrip(")")
+def findPattern(turns, nodes, cn):
+    steps = 0
+    while cn[-1] != "Z":
+        instruction = turns[steps % len(turns)]
+        if instruction == "L":
+            cn = nodes[cn][0]
+        else:
+            cn = nodes[cn][1]
+        steps += 1
+    return steps
 
-    nodes[node] = (next1, next2)
-    if node[2] == "A":
-        p2StartNodes.append(node)
+def solve():
 
-part1(turns, nodes)
+    lines = readInput.yieldLines("8")
+
+    turns = next(lines).strip()
+    next(lines)
+
+    nodes = dict()
+    p2StartNodes = []
+    for line in lines:
+        line = line.strip()
+        node, nextNodes = line.split(" = ")
+        next1, next2 = nextNodes.split(", ")
+        next1 = next1.lstrip("(")
+        next2 = next2.rstrip(")")
+
+        nodes[node] = (next1, next2)
+        if node[-1] == "A":
+            p2StartNodes.append(node)
+
+    part1(turns, nodes)
+    part2(turns, nodes, p2StartNodes)
+
+if __name__ == "__main__":
+    solve()
