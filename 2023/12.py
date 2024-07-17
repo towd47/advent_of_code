1cd4b32793016834564fe0fd5a87746762790e3e
Tom Pescatore <tpescatore11@gmail.com>
2024-07-17 11:29:30 -0400

    12 soln

--- 2023/12.py
+++ 2023/12.py
@@ -3,13 +3,27 @@ import readInput
 def p1(lines):
     return sum(map(possibleArrangements, lines))
 
+def p2(lines):
+    return sum(map(possibleArrangements2, lines))
+
 def possibleArrangements(line):
     springs, broken = line.split()
     broken = [int(x) for x in broken.split(",")]
     springs = springs.strip(".")
     springs = ".".join([x for x in springs.split(".") if x])
     val = allPossibilities(springs, broken)
-    print(springs, broken, val)
+    return val
+
+def possibleArrangements2(line):
+    springs, broken = line.split()
+    broken = [int(x) for x in broken.split(",")]
+    broken = broken * 5
+    newsprings = list(springs)
+    for _ in range(4):
+        newsprings.append("?")
+        newsprings.extend(springs)
+    newsprings = "".join(newsprings)
+    val = allPossibilities(newsprings, broken)
     return val
 
 def allPossibilities(springs, broken):
@@ -44,13 +58,62 @@ def allPossibilities(springs, broken):
         newranges = [x for x in ranges[-1] if x + broken[-1] > lastBroken]
         ranges[-1] = newranges
 
-    return 1
+    counts = countPossibilitiesV2(ranges, broken, brokenposes)
+    _, x = list(zip(*counts))
+    total = sum(x)
 
-def countPossibilities(r, prev):
-    if prev == None:
-        return
+    return total
+
+def countPossibilitiesV2(ranges, broken, brokenposes):
+    if len(ranges) == 1:
+        return list(zip(ranges[0], [1] * len(ranges[0])))
+    
+    prev_poss = countPossibilitiesV2(ranges[1:], broken[1:], brokenposes)
+    counts = []
+    for x in ranges[0]:
+        b = broken[0]
+        tot = 0
+        for p in prev_poss:
+            if x + b >= p[0]:
+                continue
+            valid = True
+            for bp in brokenposes:
+                if bp >= x + b and bp < p[0]:
+                    valid = False
+                    break
+            if not valid:
+                break
+            tot += p[1]
+        counts.append(tot)
+    return list(zip(ranges[0], counts))
+
+def countPossibilities(start, ranges, broken, brokenposes):
+    total = 0
+    for x in ranges[0]:
+        if len(ranges) == 1:
+            if checkValid(start + [x], broken, brokenposes):
+                total += 1
+        else:
+            total += countPossibilities(start + [x], ranges[1:], broken, brokenposes)
+    return total
+    
+    
+def checkValid(possibility, broken, brokenposes):
+    for i in range(len(possibility) - 1):
+        if possibility[i] + broken[i] >= possibility[i+1]:
+            return False
+    for x in brokenposes:
+        found = False
+        for a, b in zip(possibility, broken):
+            if x >= a and x < a + b:
+                found = True
+                break
+        if not found:
+            return False
+    return True
 
 
 if __name__ == "__main__":
-    lines = readInput.linesToList("tests")
+    lines = readInput.linesToList("12")
     print(p1(lines))
+    print(p2(lines))
