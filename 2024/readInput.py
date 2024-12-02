8e758c0fec74055d127e033744a614ecdd692fa3
towd47 <tpescatore11@gmail.com>
2024-12-01 21:17:06 -0500

    Dont forget this readinput file that day1 depends on :p

---
+++ 2024/readInput.py
@@ -0,0 +1,10 @@
+def yieldLines(filename):
+	with open(f'inputs/{filename}', 'r') as f:
+		for line in f:
+			yield line
+
+def linesToList(filename):
+	with open(f'inputs/{filename}', 'r') as f:
+		lines = f.readlines()
+		lines = [line.strip() for line in lines]
+		return lines
