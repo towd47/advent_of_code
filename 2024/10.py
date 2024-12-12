509238ce6e5b769ba3058ab41aad0a18304c4efb
Tom <tpescatore11@gmail.com>
2024-12-12 17:12:05 -0500

    day10 p1 soln - got stuck not realizing coords needed __hash__ overriden to be set unique

---
+++ 2024/10.py
@@ -0,0 +1,43 @@
+from readInput import linesToList
+from coord import Coord 
+import sys
+
+def solve(filename='10'):
+    grid = linesToList(filename)
+    grid = [[int(x) for x in list(line)] for line in grid]
+    
+    startPts = []
+    for i, e in enumerate(grid):
+        for j, e2 in enumerate(e):
+            if e2 == 0:
+                startPts.append(Coord(i, j))
+
+    rows = len(grid)
+    cols = len(grid[0])
+    tot = 0
+    for pt in startPts:
+        visitedCoords = set()
+        ptsToVisit = [pt]
+        while ptsToVisit:
+            point = ptsToVisit.pop()
+            visitedCoords.add(point)
+            if point.val(grid) == 9:
+                tot += 1
+            else:
+                adjacent = [c for c in point.adjacent() if c.inBounds(rows, cols)]
+                for a in adjacent:
+                    if a.val(grid) == point.val(grid) + 1:
+                        if a in visitedCoords:
+                            pass
+                        else:
+                            ptsToVisit.append(a)
+                            visitedCoords.add(a)
+
+    print(tot)
+
+if __name__ == '__main__':
+    if len(sys.argv) >= 2:
+        filename = sys.argv[1]
+        solve(filename)
+    else:
+        solve()
---
+++ 2024/coord.py
@@ -0,0 +1,46 @@
+class Coord:
+    def __init__(self, row, col):
+        self.row = row
+        self.col = col
+
+    def fromCoord(coord):
+        return Coord(coord.row, coord.col)
+
+    def up(self):
+        return Coord(self.row - 1, self.col)
+
+    def down(self):
+        return Coord(self.row + 1, self.col)
+
+    def left(self):
+        return Coord(self.row, self.col - 1)
+
+    def right(self):
+        return Coord(self.row, self.col + 1)
+
+    def adjacent(self):
+        return [self.up(), self.right(), self.down(), self.left()]
+
+    def inBounds(self, rows, cols, row_min=0, col_min=0):
+        return self.row < rows and self.col < cols and self.row >= row_min and self.col >= col_min
+
+    def add(self, coord):
+        return Coord(self.row + coord.row, self.col + coord.col)
+
+    def sub(self, coord):
+        return Coord(self.row - coord.row, self.col - coord.col)
+        
+    def val(self, grid):
+        return grid[self.row][self.col]
+
+    def __str__(self):
+        return f"{self.row}, {self.col}"
+
+    def __eq__(self, coord):
+        return self.row == coord.row and self.col == coord.col
+
+    def __attrs(self):
+        return (self.row, self.col)
+
+    def __hash__(self):
+        return hash(self.__attrs())
