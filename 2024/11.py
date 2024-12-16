22216a8c69fc8bf8d57256fbe1b2adae15110824
Tom <tpescatore11@gmail.com>
2024-12-15 20:02:49 -0500

    day11 p2 soln - shoulda though of this earlier!

--- 2024/11.py
+++ 2024/11.py
@@ -3,30 +3,29 @@ import sys
 
 def solve(filename='11'):
 	line = oneString(filename).strip().split()
-	valmap = dict()
 
-	print(sum([doSteps(25, 0, val, valmap) for val in line]))
-	print(sum([doSteps(75, 0, val, valmap) for val in line]))
-	# for key in valmap:
-	# 	print(key, valmap[key])
+	for _ in range(25):
+		vals = fullStep(vals)
 
-def doSteps(totSteps, steps, val, valmap):
-	if val not in valmap:
-		valmap[val] = computeSteps(val)
-	s, vs = valmap[val]
-	if steps + s > totSteps:
-		return 1
-	if steps + s == totSteps:
-		return 2
-	return sum([doSteps(totSteps, steps + s, v1, valmap) for v1 in vs])
+	print(sum([vals[v] for v in vals]))
 
-def computeSteps(val):
-	next_vals = step(val)
-	steps = 1
-	while len(next_vals) == 1:
-		next_vals = step(next_vals[0])
-		steps += 1
-	return steps, next_vals
+	for _ in range(50):
+		vals = fullStep(vals)
+
+	print(sum([vals[v] for v in vals]))
+
+
+def fullStep(vals):
+	newVals = dict()
+	for val in vals:
+		amount = vals[val]
+		nextStep = step(val)
+		for v in nextStep:
+			if v in newVals:
+				newVals[v] += amount
+			else:
+				newVals[v] = amount
+	return newVals
 
 def step(val):
 	if int(val) == 0:
@@ -42,8 +41,6 @@ def step(val):
 		return [v1, v2]
 
 	return [str(int(val) * 2024)]
-
-
 
 if __name__ == '__main__':
     if len(sys.argv) >= 2:
