aa1f13b772fe41e0b2421311095b063e99370571
Tom <tpescatore11@gmail.com>
2024-12-17 17:24:32 -0500

    day16 p2 soln!

--- 2024/16.py
+++ 2024/16.py
@@ -18,29 +18,71 @@ def solve(filename='16'):
 
     facing = '>'
 
-    print(p1(startPos, endPos, facing, walls, grid))
+    score, path = p1(startPos, endPos, facing, walls)
+    print(score)
+    p2(score, startPos, endPos, facing, walls)
 
-def p1(startPos, endPos, facing, walls, grid):
+def p1(startPos, endPos, facing, walls):
     pq = []
     score = 0
     visited = set()
     visited.add(startPos)
-    heapq.heappush(pq, (priorityCalc(startPos, endPos, score), (startPos, facing, score)))
+    heapq.heappush(pq, (priorityCalc(startPos, endPos, score), (startPos, facing, score, [(startPos, facing, score)])))
     
     while pq:
-        pt, facing, score = heapq.heappop(pq)[1]
+        pt, facing, score, path = heapq.heappop(pq)[1]
         if pt == endPos:
-            return score
+            return score, path
         adj = [adj for adj in pt.adjacent() if adj not in walls and adj not in visited]
         for a in adj:
             if getByFacing(pt, a) == facing:
                 newScore = score + 1
-                heapq.heappush(pq, (priorityCalc(a, endPos, newScore), (a, facing, newScore)))
+                heapq.heappush(pq, (priorityCalc(a, endPos, newScore), (a, facing, newScore, path + [(a, facing, newScore)])))
             else:
                 newScore = score + 1001
-                heapq.heappush(pq, (priorityCalc(a, endPos, newScore), (a, getByFacing(pt, a), newScore)))
+                heapq.heappush(pq, (priorityCalc(a, endPos, newScore), (a, getByFacing(pt, a), newScore, path + [(a, getByFacing(pt, a), newScore)])))
             visited.add(a)
 
+def p2(bestScore, startPos, endPos, facing, walls):
+    pq = []
+    score = 0
+    visitedDict = {}
+    visitedDict[(startPos, facing)] = score
+    bestPaths = set()
+    heapq.heappush(pq, (priorityCalc(startPos, endPos, score), (startPos, facing, score, [startPos])))
+    
+    while pq:
+        # for val in visitedDict:
+        #     print(val[0], val[1], visitedDict[val]) 
+        # input()
+        pt, facing, score, path = heapq.heappop(pq)[1]
+        # for i in range(15):
+        #     line = []
+        #     for j in range(15):
+        #         if Coord(i, j) in walls:
+        #             line.append('#')
+        #         elif Coord(i, j) in path:
+        #             line.append('O')
+        #         else:
+        #             line.append('.')
+        #     print("".join(line))
+        # input()    
+        if pt == endPos:
+            bestPaths.update(path)
+        adj = [adj for adj in pt.adjacent() if adj not in walls]
+        for a in adj:
+            if getByFacing(pt, a) == facing:
+                newScore = score + 1
+            else:
+                newScore = score + 1001
+            newFacing = getByFacing(pt, a)
+            if (a, newFacing) in visitedDict and newScore > visitedDict[(a, newFacing)]:
+                continue
+            visitedDict[(a, newFacing)] = newScore
+            if priorityCalc(a, endPos, newScore) <= bestScore:
+                heapq.heappush(pq, (priorityCalc(a, endPos, newScore), (a, getByFacing(pt, a), newScore, path + [a])))
+    print(len(bestPaths))
+ 
 def getByFacing(pt, adj):
     if pt.row > adj.row:
         return '^'
