e0b212a10ca4f510955c98f3cac9a5474a8444df
Tom <tpescatore11@gmail.com>
2024-12-26 09:53:18 -0800

    day 22 full soln

--- 2024/22.py
+++ 2024/22.py
@@ -3,7 +3,18 @@ import sys
 
 def solve(filename='22'):
     lines = yieldLines(filename)
-    print(sum([nextNSecrets(int(secret.strip()), 2000) for secret in lines]))
+    lines = [int(line.strip()) for line in lines]
+    print(sum([nextNSecrets(secret, 2000) for secret in lines]))
+    diffMap = {}
+    for secret in lines:
+        dm = genDiffMap(secret, 1999)
+        for key in dm:
+            if key in diffMap:
+                diffMap[key] += dm[key]
+            else:
+                diffMap[key] = dm[key]
+
+    print(max(diffMap.values()))
 
 def nextNSecrets(secret, n):
     [secret := nextSecret(secret) for _ in range(n)]
@@ -15,6 +26,21 @@ def nextSecret(secret):
     secret = (secret ^ (secret * 2048)) % 16777216
     return secret
 
+def genDiffMap(secret, n):
+    diffs = []
+    diffMap = {}
+    for i in range(n):
+        s = secret
+        s1 = nextSecret(secret)
+        diffs.append(s1 % 10 - s % 10)
+        if len(diffs) == 4:
+            diffTup = tuple(diffs)
+            if diffTup not in diffMap:
+                diffMap[diffTup] = s1 % 10
+            diffs.pop(0)
+        secret = s1
+    return diffMap
+
 if __name__ == '__main__':
     if len(sys.argv) >= 2:
         solve(sys.argv[1])
@@ -23,3 +49,5 @@ if __name__ == '__main__':
 
 
 # 14337231886 high
+# 903 low
+# 1715 high
